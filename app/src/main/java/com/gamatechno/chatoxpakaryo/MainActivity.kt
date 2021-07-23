9c7ef326274095c4f97f0049fe251fc25a7d1624
Ardi Baskoro <ardi@gamatechno.com>
2021-07-23 16:31:34 +0700

    change material theme

--- app/src/main/java/com/gamatechno/chatoxpakaryo/MainActivity.kt
+++ app/src/main/java/com/gamatechno/chatoxpakaryo/MainActivity.kt
@@ -2,10 +2,15 @@ package com.gamatechno.chatoxpakaryo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gamatechno.chato.sdk.app.main.ChatoFragmentNew
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*supportFragmentManager.beginTransaction()
+            .replace(R.id.flMain, ChatoFragmentNew())
+            .commitAllowingStateLoss()*/
     }
 }
